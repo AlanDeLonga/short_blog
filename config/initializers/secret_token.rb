e173046c3d4b87746831f34bc410dbe383fce4dd
adelonga <cscatcalpoly@yahoo.com>
2012-08-30 15:48:53 -0700

    Initial commit of small blog app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShortBlog::Application.config.secret_token = '283fcecc6e8859007912ca179cf17298e2e583b94e39444e02832d46bd4f806cdf1d560e4187f8f9047ee3e3e246d3cd5cc885604d4c30e43cdd6f4b1c12aa4d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ShortBlog::Application.config.session_store :cookie_store, key: '_short_blog_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ShortBlog::Application.config.session_store :active_record_store
