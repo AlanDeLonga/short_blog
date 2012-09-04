67f15160cd67f1ad78d855f15dfbbf46dbbb0d9e
adelonga <cscatcalpoly@yahoo.com>
2012-09-04 16:08:50 -0700

    finishing simple blog example using ruby scaffolding

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,3 +1,10 @@
 class Micropost < ActiveRecord::Base
+	
+  # setting up one to many association between users and micropost	
+  belongs_to :user
+
   attr_accessible :content, :user_id
+  
+  # restricting the amount of characters that can be input into content field
+  validates :content, :length => { :maximum =>140 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
+
+  # setting up many to one association between microposts and users
+  has_many :microposts
+
   attr_accessible :email, :name
 end
